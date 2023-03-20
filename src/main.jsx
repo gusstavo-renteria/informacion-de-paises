8ee4e38bcca8a6d49c7cb8705f6cafb5c9ba7cf2
gusstavo-renteria <guss.renteria.g@gmail.com>
2023-03-19 20:25:48 -0600

    +primer 'commit'

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import './styles/global.css'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <main>
+    <h1>ww4x's-land</h1>
+  </main>
+)
